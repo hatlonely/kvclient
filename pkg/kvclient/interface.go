dcc9f89868525d728b672a2a9ad258720a877914
hatlonely <hatlonely@gmail.com>
2018-06-18 15:50:25 +0800

    update

--- pkg/kvclient/interface.go
+++ pkg/kvclient/interface.go
@@ -31,10 +31,10 @@ type KVClient interface {
 
 // Cache interface
 type Cache interface {
-	Get(key string) (val []byte, err error) // return nil, nil if key not found
-	Set(key string, val []byte) (err error) // key will expire with default configuration
-	Del(key string) (err error)
-	SetBatch(keys []string, vals [][]byte) (errs []error, err error)
+	Get(key string) ([]byte, error)   // return nil, nil if key not found
+	Set(key string, val []byte) error // key will expire with default configuration
+	Del(key string) error
+	SetBatch(keys []string, vals [][]byte) ([]error, error)
 	SetEx(key string, val []byte, expiration time.Duration) error   // set with expiration
 	SetNx(key string, val []byte) error                             // set if not exists
 	SetExNx(key string, val []byte, expiration time.Duration) error // set if not exists with expiration
